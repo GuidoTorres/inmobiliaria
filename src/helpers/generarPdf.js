7e8558ada8821779534e70a4bf5bce84d086c245
Hector <gt12930@GMAIL.com>
2023-06-26 08:44:34 -0500

    fix: descarga cotizacion

--- src/helpers/generarPdf.js
+++ src/helpers/generarPdf.js
@@ -1,28 +1,25 @@
-const puppeteer = require("puppeteer");
+const pdf = require("html-pdf");
 
 const generarPDF = async (html) => {
-    try {
-      const browser = await puppeteer.launch({ headless: "new" });
-      const page = await browser.newPage();
-  
-      await page.setContent(html, { waitUntil: "networkidle0" });
-  
-      const pdf = await page.pdf({
-        format: "A4",
-        printBackground: true,
-        margin: {
-          top: "0px",
-          right: "0px",
-          bottom: "0px",
-          left: "0px",
-        },
-      });
-  
-      await browser.close();
-      return pdf;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  return new Promise((resolve, reject) => {
+    const options = {
+      format: "A4",
+      border: {
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
+      },
+    };
+
+    pdf.create(html, options).toBuffer((err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buffer);
+      }
+    });
+  });
+};
 
   module.exports= generarPDF
