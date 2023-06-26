97845be77bce7979d6c7a34d6b2ac7843329d7f7
Hector <gt12930@GMAIL.com>
2023-06-26 09:18:43 -0500

    fix: descarga cotizacion

--- src/helpers/generarPdf.js
+++ src/helpers/generarPdf.js
@@ -3,6 +3,7 @@ const pdf = require("html-pdf");
 const generarPDF = async (html) => {
   return new Promise((resolve, reject) => {
     const options = {
+      "phantomPath": "./node_modules/phantomjs-prebuilt/bin/phantomjs", 
       format: "A4",
       border: {
         top: "0px",
