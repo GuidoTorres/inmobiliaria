90bb366fe02d8060eb18dda7ce8af1237fb92215
Hector <gt12930@GMAIL.com>
2023-06-27 11:35:11 -0500

    fix: eliminar usuario

--- src/controllers/registroInicioSesion.js
+++ src/controllers/registroInicioSesion.js
@@ -7,36 +7,32 @@ const get = async (req, res) => {
       where:{cod_rol: 2},
       include: [{ model: RegistroInicioSesion }],
     });
-    let iteracion = 1; 
-    const formatData = usuarios.flatMap((usuario) =>
-    usuario.registro_inicio_sesions.map((ingreso, a) => {
-      const item = {
-        nro: iteracion, // Agregar el número de iteración consecutivo
-        id: iteracion,
+    const formatData = usuarios.flatMap((usuario, i) => 
+      usuario.registro_inicio_sesions.map((ingreso, a) => ({
+        id: ingreso.cod_registro,
         cod_usuario: usuario.cod_usuario,
         nombre: usuario.nombre,
         fecha_ingreso: dayjs(ingreso.fecha_ingreso).format("DD-MM-YYYY"),
         hora_ingreso: ingreso.hora_ingreso,
-      };
-  
-      iteracion++; // Incrementar el número de iteración para la siguiente iteración
-  
-      return item;
-    })
-  )
-  // .sort((a, b) => {
-  //     const fechaA = dayjs(a.fecha_ingreso, "DD-MM-YYYY");
-  //     const fechaB = dayjs(b.fecha_ingreso, "DD-MM-YYYY");
+      }))
+    ).sort((a, b) => {
+      const fechaA = dayjs(a.fecha_ingreso, "DD-MM-YYYY");
+      const fechaB = dayjs(b.fecha_ingreso, "DD-MM-YYYY");
     
-  //     if (fechaA.isBefore(fechaB)) {
-  //       return -1;
-  //     } else if (fechaA.isAfter(fechaB)) {
-  //       return 1;
-  //     } else {
-  //       // Las fechas son iguales, ordenar por hora_ingreso
-  //       return a.hora_ingreso.localeCompare(b.hora_ingreso);
-  //     }
-  //   });
+      if (fechaA.isBefore(fechaB)) {
+        return -1;
+      } else if (fechaA.isAfter(fechaB)) {
+        return 1;
+      } else {
+        // Las fechas son iguales, ordenar por hora_ingreso
+        return a.hora_ingreso.localeCompare(b.hora_ingreso);
+      }
+    }).map((item, i) => {
+      return{
+        nro: i,
+        ...item
+      }
+    });
 
     return res.status(200).json({ data: formatData });
   } catch (error) {
