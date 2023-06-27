ce9492fcab38ae6d0fee88211bc09c5631b3bb1d
Hector <gt12930@GMAIL.com>
2023-06-27 11:27:39 -0500

    fix: eliminar usuario

--- src/controllers/registroInicioSesion.js
+++ src/controllers/registroInicioSesion.js
@@ -7,17 +7,23 @@ const get = async (req, res) => {
       where:{cod_rol: 2},
       include: [{ model: RegistroInicioSesion }],
     });
-
-    const formatData = usuarios.flatMap((usuario, i) => 
-      usuario.registro_inicio_sesions.map((ingreso, a) => ({
-        nro: i+1,
+    let iteracion = 1; 
+    const formatData = usuarios.flatMap((usuario) =>
+    usuario.registro_inicio_sesions.map((ingreso, a) => {
+      const item = {
         id: ingreso.cod_registro,
         cod_usuario: usuario.cod_usuario,
         nombre: usuario.nombre,
         fecha_ingreso: dayjs(ingreso.fecha_ingreso).format("DD-MM-YYYY"),
         hora_ingreso: ingreso.hora_ingreso,
-      }))
-    ).sort((a, b) => {
+        iteracion: iteracion, // Agregar el número de iteración consecutivo
+      };
+  
+      iteracion++; // Incrementar el número de iteración para la siguiente iteración
+  
+      return item;
+    })
+  ).sort((a, b) => {
       const fechaA = dayjs(a.fecha_ingreso, "DD-MM-YYYY");
       const fechaB = dayjs(b.fecha_ingreso, "DD-MM-YYYY");
     
