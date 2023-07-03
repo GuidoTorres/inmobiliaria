c64dc7a738804cf5f9c4d78c8b11c2b199576b4c
Hector <gt12930@GMAIL.com>
2023-07-02 20:32:05 -0500

    fix: descarga

--- src/controllers/cotizacion.js
+++ src/controllers/cotizacion.js
@@ -235,7 +235,7 @@ const descargarCotizacion = async (req, res) => {
     // Lanza una nueva instancia de Puppeteer
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: "new", timeout: 0
+      headless: "new", timeout: 6000
     });
 
     const page = await browser.newPage();
